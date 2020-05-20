9a4994b3b7579827ab89a61693b85e21c6d8b1a1
Yesat <lionel.ieri@gmail.com>
2020-05-20 13:11:52 +0200

    Runs way faster

--- greedy.py
+++ greedy.py
@@ -3,52 +3,61 @@ import matplotlib.pyplot as plt
 import random
 from tqdm import tqdm
 
-
-def icm(G, nodes):
-    actives = nodes # a node in nodes can't be reactivated
-    activated = []
-    for n in nodes:
+def neighbors_activation(G):
+    neighbor_of = {}
+    for n in G.nodes():
+        activated = []
         for neighbor in G.neighbors(n):
             weight = G.get_edge_data(n, neighbor).get("weight")
-            randomValue = random.uniform(0,1)
-            if (randomValue < weight) and neighbor not in actives: # check if it's already active
+            randomValue = random.uniform(0, 1)
+            if (randomValue < weight): 
                 activated.append(neighbor)
-                actives.append(neighbor)
+        neighbor_of[n]= activated
+    return neighbor_of
+
+def icm(G, nodes, act):
+    actives = nodes # a node in nodes can't be reactivated
+    activated = set()
+    for n in nodes:
+        for neighbor in G.neighbors(n):
+            if neighbor in act[n]:
+                activated.add(neighbor)
     return len(set(activated))
 
 #this will not work, we have to keep it in the main function (greed())
-def bestNode(G, list_node):
-    print(list_node)
-    best_node = list_node[0]
-    length = 0
-    for node in list_node:
-        numberOfActivation = icm(G, node)
-        print("Length : " +  str(length) + " Number of activation: " + str(numberOfActivation))
-        if numberOfActivation > length:
-            prev_best = best_node
-            best_node = node
-            length = numberOfActivation
-            #length muss übergeben werden
-    print("THIS is the best node: " + str(list(bestNode)))
-    return best_node
+# def bestNode(G, list_node):
+#     print(list_node)
+#     best_node = list_node[0]
+#     length = 0
+#     for node in list_node:
+#         numberOfActivation = icm(G, node)
+#         print("Length : " +  str(length) + " Number of activation: " + str(numberOfActivation))
+#         if numberOfActivation > length:
+#             prev_best = best_node
+#             best_node = node
+#             length = numberOfActivation
+#             #length muss übergeben werden
+#     print("THIS is the best node: " + str(list(bestNode)))
+#     return best_node
 
-def greedy(budget, list_of_nodes):
+def greedy(budget, G):
     i = 0
     k = budget
     Seed = []
-    nodeList = list(list_of_nodes)
     length = 0
     random.seed(4)  # Seed chosen by fair dice roll, guaranteed to be random. https://xkcd.com/212
     
+    nodeList = list(G.nodes())
+    act = neighbors_activation(G)
     
     while i != k:
         #best_node = bestNode(G, nodeList)
         #nodeList.remove(best_node)
         best_node = nodeList[0]
         numberOfActivations = {}
-        for node in tqdm(nodeList, 'searching for the %d most activating user'% i+1):
+        for node in tqdm(nodeList, 'searching for the %d most activating user'% (i+1)):
             fSuV = Seed + [node]
-            numberOfActivation = icm(G, fSuV)
+            numberOfActivation = icm(G, fSuV,act)
             # print("Length : " +  str(length) + " Number of activation: " + str(numberOfActivation))
             # if numberOfActivation > length:
             #     prev_best = best_node
@@ -74,6 +83,6 @@ G = nx.read_weighted_edgelist('normalized.edgelist', create_using=nx.DiGraph())
 list_nodes = list(G.nodes())
 
 print(list_nodes[0])
-bestSeed = greedy(10, list_nodes)
+bestSeed = greedy(10, G)
 
 print(set(bestSeed))

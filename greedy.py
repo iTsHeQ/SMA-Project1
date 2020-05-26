c8678f62158d5c09b8a3204605ed93988174c9c6
Yesat <lionel.ieri@gmail.com>
2020-05-26 12:35:32 +0200

    fixed graph

--- greedy.py
+++ greedy.py
@@ -19,26 +19,30 @@ def neighbors_activation(G,upper):
         neighbors_of[n]= activated
     return neighbors_of
 
-def cascades_gen(G, neighbours_activation):
+def cascades_gen(G, activations):
     icm = {}
+    icm_list = {}
     for node in tqdm(G.nodes(),'modeling of the cascades for each nodes'):
         actives, passives = [node],[]
         while bool(actives):
             for n in actives:
                 activated = []
                 for neighbor in (set(G.neighbors(n)) - set(passives)):
-                    if neighbor in neighbours_activation[n]:
+                    if neighbor in activations[n]:
                         activated.append(neighbor)
             passives +=actives
             actives = activated
         icm[node] = set(passives)
+        icm_list[node] = passives
     sorted_icm = {k: v for k, v in sorted(
         icm.items(), key=lambda item: len(item[1]), reverse=True)}
-    return(sorted_icm)
+    results = [sorted_icm, icm_list]
+    return(results)
 
 
 def pregen_icm(nodes, cascades):
-    activated_per_node = {node: cascades[node] for node in nodes}
+    _cascades = cascades[0]
+    activated_per_node = {node: _cascades[node] for node in nodes}
     values = activated_per_node.values()
     activated = set.union(*values)
     size = len(activated)
@@ -46,14 +50,16 @@ def pregen_icm(nodes, cascades):
 
 # This function takes three arguments, and calculated the best possible starting nodes
 
-def greedy(budget, G, act, cascades):
+def greedy(budget, G, cascades):
     i = 0
     Seed = []
     random.seed(4)  # Seed chosen by fair dice roll, guaranteed to be random. https://xkcd.com/212 
     nodeList = list(G.nodes())
+    _cascades = cascades[0]
     while i != budget:
         numberOfActivations = {}
-        for node in tqdm(nodeList, 'searching for the %d/%d most activating user'% (i+1,budget)):
+        # for node in tqdm(nodeList, 'searching for the %d/%d most activating user'% (i+1,budget)):
+        for node in nodeList:
             SuV = Seed + [node]
             numberOfActivation = pregen_icm(SuV, cascades)
             numberOfActivations[node] = numberOfActivation
@@ -64,30 +70,32 @@ def greedy(budget, G, act, cascades):
     # seed_activation = pregen_icm(Seed,cascades,True)
     return Seed
 
-def greedyPlotting(Graph, act, cas):
+def greedyPlotting(Graph, cascades):
     greedyPlot = {}
     listNumberActivated = []
     random.seed(4)
     lengthICM = 0
-    for i in range(9, 30, 10):
-        seed = greedy(i, Graph, act, cas)
-        lengthICM = 0
-        for numberOfActivation in seed.values():
-            lengthICM += numberOfActivation
+    for i in range(9, 100, 10):
+        seed = greedy(i, Graph, cascades)
+        lengthICM = pregen_icm(seed, cascades)
+        # lengthICM = 0
+        # for numberOfActivation in seed.values():
+        #     lengthICM += numberOfActivation
         greedyPlot[i] = lengthICM
+        print(lengthICM)
     
     x,y = zip(*sorted(greedyPlot.items()))
     plt.plot(x,y)
     plt.show()
     
-def icmPlotting(seed, cas):
-    nActiv = pregen_icm(seed, cas)
+def icmPlotting(seed, cascades):
+    nActiv = pregen_icm(seed, cascades)
     icmPlot = {}
     total = 0
     for i in range(0, nActiv, 1):
         for node in seed:
             try:
-                cas[node][i]
+                cascades[1][node][i]
             except:
                 continue
             else:
@@ -106,25 +114,19 @@ def data_treatment():
     G = nx.read_weighted_edgelist(
         norm_edge, nodetype=int, create_using=nx.DiGraph())
     upper = 1
-    activations_name = 'activations_0-%i.pkl' % upper
-    cascades_name = 'cascades0-%i.pkl' % upper
-    activations_file = os.path.join(preproc, activations_name)
+    cascades_name = 'cascades_0-%i.pkl' % upper
     cascades_file = os.path.join(preproc, cascades_name)
-    if os.path.isfile(activations_file):
-        f = open(activations_file, 'rb')
-        activations = pickle.load(f)
+    if os.path.isfile(cascades_file):
+        print('hey listen')
         g = open(cascades_file, 'rb')
         cascades = pickle.load(g)
     else:
         activations = neighbors_activation(G, upper)
         cascades = cascades_gen(G, activations)
-        f = open(activations_file, 'wb')
-        pickle.dump(activations, f)
         g = open(cascades_file, 'wb')
         pickle.dump(cascades, g)
-    f.close()
     g.close()
-    return(G, cascades, activations)
+    return(G, cascades)
 
 def data_treatment_test():
     dataset_loc = 'testing'
@@ -155,10 +157,10 @@ def data_treatment_test():
 
 
 def main():
-    G, cascades, activations = data_treatment()
-    bestSeed = greedy(5, G, activations, cascades)
+    G, cascades = data_treatment()
+    bestSeed = greedy(30, G, cascades)
     icmPlotting(bestSeed, cascades)
-    greedyPlotting(G, activations, cascades)
+    greedyPlotting(G, cascades)
     #print(list(bestSeed.values()))
 
 if __name__ == '__main__':
